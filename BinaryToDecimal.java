c7fec9c234a5acce367bf91fae13ea35e94ed6f6
Abrar Ahmad Tausif <tausifahmadabrar@gmail.com>
2026-08-14 15:16:41 +0600

    decimal to ocatal converter

---
+++ BinaryToDecimal.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class BinaryToDecimal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Input a binary number: ");
+        String binary = scanner.next();
+        
+        int decimal = Integer.parseInt(binary, 2);
+        
+        System.out.println("Decimal Number: " + decimal);
+        scanner.close();
+    }
+}
---
+++ DecimalToOctal.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class DecimalToOctal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Input a Decimal Number: ");
+        int decimal = scanner.nextInt();
+        
+        String octal = Integer.toOctalString(decimal);
+        
+        System.out.println("Octal number is: " + octal);
+        scanner.close();
+    }
+}
